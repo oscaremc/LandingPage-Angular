64318e346422d2900ab71e7e88d0aaa63f15386d
Oscar Eduardo Montalvo Camacho <45255751+oscaremc@users.noreply.github.com>
2022-02-05 17:05:58 -0500

    se carga primera impresion de la web sencilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { CommonModule } from '@angular/common';
+import { MapaComponent } from './mapa/mapa.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InfoDetallesComponent } from './info-detalles/info-detalles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CarouselComponent,
+    GaleriaComponent,
+    MapaComponent,
+    ContactoComponent,
+    InfoDetallesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/info-detalles/info-detalles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-detalles',
+  templateUrl: './info-detalles.component.html',
+  styleUrls: ['./info-detalles.component.css']
+})
+export class InfoDetallesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

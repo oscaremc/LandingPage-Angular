0cc87b58cc1f7d21c8388a14c8cf4e656ccfff01
Oscar Eduardo Montalvo Camacho <45255751+oscaremc@users.noreply.github.com>
2022-02-27 21:30:56 -0500

    se cambia ruta 1

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/fincabellavista.herokuapp.com'));
+app.use(express.static(__dirname + '/dist/web-bella-vista'));
+
+// intentos
+// web-BellaVista
+// web-bella-vista
+// fincabellavista.herokuapp.com
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/fincabellavista.herokuapp.com/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/web-bella-vista/index.html'));
 });
 
 // default Heroku port
